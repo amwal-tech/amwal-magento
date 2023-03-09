dab379a2e609cacd4bace07addc67f2488242d0a
Martijn Swinkels <m.swinkels@youweagency.com>
2023-03-09 16:37:11 +0100

    AMWAL-42 | Added error handling through amwalPrePayTriggerError event

---
+++ view/frontend/web/js/model/error-handler.js
@@ -0,0 +1,39 @@
+define([
+    'jquery',
+    'Magento_Customer/js/customer-data'
+],
+function ($, customerData) {
+    'use strict';
+
+    return {
+
+        /**
+         * Process an error message and send the associated event to the Amwal modal
+         * @return {String}
+         */
+        process: function(message) {
+            message = message || this.getDefaultErrorMessage();
+
+            window.dispatchEvent(new CustomEvent('amwalPrePayTriggerError', {
+                detail: {
+                    description: message
+                }
+            }));
+
+            customerData.set('messages', {
+                'messages': [{
+                    'type': 'error',
+                    'text': message
+                }]
+            });
+        },
+
+        /**
+         * Return the translated default error message.
+         * @return {String}
+         */
+        getDefaultErrorMessage: function() {
+            return $.mage.__('Something went wrong while placing your order.');
+        }
+    }
+});
