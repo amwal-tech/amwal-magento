5876e5ed14088b5a72dedfaa2fa96517120a57c5
Ahmed Mardi <141551822+ahmardi@users.noreply.github.com>
2023-10-11 19:43:16 +0300

    Add Client info to addressData (#158)
    * Add Client info to addressData
    
    * WIP
    
    * Update GetQuote.php
    
    * try merge

--- Model/Checkout/GetQuote.php
+++ Model/Checkout/GetQuote.php
@@ -145,12 +145,12 @@ class GetQuote extends AmwalCheckoutAction
 
             if (!$isPreCheckout) {
                 $amwalOrderData = $this->objectFactory->create([
-                    'client_first_name' => AddressResolver::TEMPORARY_DATA_VALUE,
-                    'client_last_name' => AddressResolver::TEMPORARY_DATA_VALUE,
-                    'client_phone_number' => AddressResolver::TEMPORARY_DATA_VALUE
+                    'client_first_name' => $addressData->getClientFirstName(),
+                    'client_last_name' => $addressData->getClientLastName(),
+                    'client_phone_number' => $addressData->getClientPhone(),
+                    'client_email' => $addressData->getClientEmail(),
                 ]);
                 $amwalOrderData->setAddressDetails($addressData);
-
                 $customerAddress = $this->getCustomerAddress($amwalOrderData, $refId);
             }
 
@@ -301,6 +301,10 @@ class GetQuote extends AmwalCheckoutAction
                 $amwalOrderData->toJson()
             ));
             $customerAddress = $this->addressResolver->execute($amwalOrderData);
+            $this->logDebug(sprintf(
+                'Resolved customer address with data: %s',
+                json_encode($customerAddress->__toArray(), JSON_THROW_ON_ERROR)
+            ));
         } catch (LocalizedException|RuntimeException $e) {
             $message = sprintf(
                 'Unable to resolve customer address with Data %s. Received exception %s',
@@ -311,16 +315,6 @@ class GetQuote extends AmwalCheckoutAction
             $this->logger->error($message);
             $this->throwException(__('Something went wrong while processing you address information.'));
         }
-
-        try {
-            $this->logDebug(sprintf(
-                'Resolved customer address with data: %s',
-                json_encode($customerAddress->__toArray(), JSON_THROW_ON_ERROR)
-            ));
-        } catch (JsonException $e) {
-            $this->logger->notice('Unable to log resolved customer address debug message');
-        }
-
         return $customerAddress;
     }
 
