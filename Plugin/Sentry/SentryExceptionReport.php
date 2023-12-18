468747cdfb440fc4a73736883dd38e545f01af1f
Ahmed Mardi <141551822+ahmardi@users.noreply.github.com>
2023-12-18 12:44:36 +0200

    Update SentryExceptionReport.php (#245)

--- Plugin/Sentry/SentryExceptionReport.php
+++ Plugin/Sentry/SentryExceptionReport.php
@@ -35,10 +35,9 @@ class SentryExceptionReport
         if (!class_exists(Sentry\ClientBuilder::class) || !$this->config->isSentryReportEnabled()) {
             return;
         }
-
         Sentry\configureScope(function (Scope $scope) {
             // Add extra context data to the exception
-            $scope->setExtra('domain', $_SERVER['HTTP_HOST']);
+            $scope->setExtra('domain', $_SERVER['HTTP_HOST'] ?? 'runtime cli');
             $scope->setExtra('plugin_type', 'magento2');
             $scope->setExtra('plugin_version', Config::MODULE_VERSION);
             $scope->setExtra('php_version', phpversion());
