9dd5f8dade234fe928eee49f609ed806354180d1
Martijn Swinkels <martiniswink@gmail.com>
2024-02-27 14:49:35 +0100

    Update github integration test workflow

---
+++ .github/workflows/config/post-install-config.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+cd $MAGENTO_ROOT
+
+echo "Configure environment and Amwal plugin"
+
+# Set required config values
+php bin/magento config:set currency/options/base SAR
+php bin/magento config:set currency/options/default SAR
+php bin/magento config:set currency/options/allow SAR
+php bin/magento config:set admin/security/admin_account_sharing 1
+php bin/magento config:set admin/security/use_form_key 0
+php bin/magento config:set cms/wysiwyg/enabled disabled
+php bin/magento config:set payment/amwal_payments/active 1
+php bin/magento config:set payment/amwal_payments/express_checkout_active 1
+php bin/magento config:set payment/amwal_payments/regular_checkout_active 1
+php bin/magento config:set payment/amwal_payments/country_code SA
+php bin/magento config:set payment/amwal_payments/dark_mode 0
+php bin/magento config:set payment/amwal_payments/order_creation_status pending_payment
+php bin/magento config:set payment/amwal_payments/order_confirmed_status processing
+php bin/magento config:set payment/amwal_payments/allowspecific 0
+php bin/magento config:set payment/amwal_payments/specificcountry
+php bin/magento config:set payment/amwal_payments/debug_mode 0
+php bin/magento config:set payment/amwal_payments/express_checkout_title Buy now with Amwal
+php bin/magento config:set payment/amwal_payments/merchant_mode live
+php bin/magento config:set payment/amwal_payments/hide_proceed_to_checkout 1
+php bin/magento config:set payment/amwal_payments/merchant_id_valid 1
+php bin/magento config:set payment/amwal_payments/create_user_on_order 1
+php bin/magento config:set payment/amwal_payments/use_base_currency 0
+php bin/magento config:set payment/amwal_payments/use_system_country_settings 1
+php bin/magento config:set payment/amwal_payments/merchant_id sandbox-amwal-e09ee380-d8c7-4710-a6ab-c9b39c7ffd47
+php bin/magento config:sensitive:set payment/amwal_payments/ref_id_secret 1234567890
+
+# Flush cache
+php bin/magento cache:flush
+
+echo "Finished configuration for environment and Amwal plugin"
