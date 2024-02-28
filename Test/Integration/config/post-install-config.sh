315bec76e14e328154775bf8381110f0c416e24d
Martijn Swinkels <martiniswink@gmail.com>
2024-02-28 18:10:28 +0100

    Update github integration test workflow

--- Test/Integration/config/post-install-config.sh
+++ Test/Integration/config/post-install-config.sh
@@ -16,18 +16,18 @@ php bin/magento config:set payment/amwal_payments/express_checkout_active 1
 php bin/magento config:set payment/amwal_payments/regular_checkout_active 1
 php bin/magento config:set payment/amwal_payments/country_code SA
 php bin/magento config:set payment/amwal_payments/dark_mode 0
-php bin/magento config:set payment/amwal_payments/order_confirmed_status processing
+php bin/magento config:set payment/amwal_payments/order_confirmed_status "processing"
 php bin/magento config:set payment/amwal_payments/allowspecific 0
 php bin/magento config:set payment/amwal_payments/debug_mode 0
-php bin/magento config:set payment/amwal_payments/express_checkout_title Buy now with Amwal
+php bin/magento config:set payment/amwal_payments/express_checkout_title "Buy now with Amwal"
 php bin/magento config:set payment/amwal_payments/merchant_mode live
 php bin/magento config:set payment/amwal_payments/hide_proceed_to_checkout 1
 php bin/magento config:set payment/amwal_payments/merchant_id_valid 1
 php bin/magento config:set payment/amwal_payments/create_user_on_order 1
 php bin/magento config:set payment/amwal_payments/use_base_currency 0
 php bin/magento config:set payment/amwal_payments/use_system_country_settings 1
-php bin/magento config:set payment/amwal_payments/merchant_id sandbox-amwal-e09ee380-d8c7-4710-a6ab-c9b39c7ffd47
-php bin/magento config:sensitive:set payment/amwal_payments/ref_id_secret 1234567890
+php bin/magento config:set payment/amwal_payments/merchant_id "sandbox-amwal-e09ee380-d8c7-4710-a6ab-c9b39c7ffd47"
+php bin/magento config:sensitive:set payment/amwal_payments/ref_id_secret "1234567890"
 php bin/magento cache:flush
 
 echo "Finished Amwal plugin configuration"
