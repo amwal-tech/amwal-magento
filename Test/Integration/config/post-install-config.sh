2ecfebe00d569d801a702263238e95d1e7ced842
Martijn Swinkels <martiniswink@gmail.com>
2024-02-29 10:42:05 +0100

    Update github integration test workflow

--- Test/Integration/config/post-install-config.sh
+++ Test/Integration/config/post-install-config.sh
@@ -29,6 +29,6 @@ php bin/magento config:set payment/amwal_payments/merchant_id "sandbox-amwal-e09
 php bin/magento cache:flush
 
 # (Re-)Compile DI
-#php bin/magento setup:di:compile
+php bin/magento setup:di:compile
 
 echo "Finished Amwal plugin configuration"
