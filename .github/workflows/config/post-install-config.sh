8cb50a56fbc0ba8c764f24c89a8f7c5085702c8a
Martijn Swinkels <martiniswink@gmail.com>
2024-03-11 12:31:46 +0100

    Update functional tests

--- .github/workflows/config/post-install-config.sh
+++ .github/workflows/config/post-install-config.sh
@@ -2,6 +2,14 @@
 
 cd $MAGENTO_ROOT
 
+echo "Enabling all modules"
+php bin/magento module:enable --all --quiet
+echo "Enabled all modules"
+
 echo "Running DI compilation"
 php bin/magento setup:di:compile --quiet
 echo "Finished DI compilation"
+
+echo "Running Setup Upgrade"
+php bin/magento setup:upgrade --quiet
+echo "Finished Setup Upgrade"
