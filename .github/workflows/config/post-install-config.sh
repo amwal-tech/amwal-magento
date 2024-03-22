fe8669adb1da799ff836fc326de7d04135e76c1e
Martijn Swinkels <martiniswink@gmail.com>
2024-03-22 16:38:35 +0100

    Update Integration tests

--- .github/workflows/config/post-install-config.sh
+++ .github/workflows/config/post-install-config.sh
@@ -8,29 +8,29 @@ sed -i 's/<integration_test_run>0<\/integration_test_run>/<integration_test_run>
 sed -i 's/<merchant_id><\/merchant_id>/<merchant_id>sandbox-amwal-e09ee380-d8c7-4710-a6ab-c9b39c7ffd47<\/merchant_id>/g' vendor/amwal/payments/etc/config.xml
 
 # Config available through Admin UI
-bin/magento config:set currency/options/allow SAR,USD -q -c
-bin/magento config:set currency/options/base SAR -q -c
-bin/magento config:set currency/options/default SAR -q -c
-bin/magento config:set currency/options/allow SAR -q -c
-bin/magento config:set admin/security/admin_account_sharing 1 -q -c
-bin/magento config:set admin/security/use_form_key 0 -q -c
-bin/magento config:set cms/wysiwyg/enabled disabled -q -c
-bin/magento config:set catalog/search/engine elasticsearch7 -q -c
-bin/magento config:set catalog/search/elasticsearch7_server_hostname es -q -c
-bin/magento config:set catalog/search/elasticsearch7_server_port 9200 -q -c
-bin/magento config:set catalog/search/elasticsearch7_server_timeout 60 -q -c
-bin/magento config:set payment/amwal_payments/active 1 -q -c
-bin/magento config:set payment/amwal_payments/dark_mode 0 -q -c
-bin/magento config:set payment/amwal_payments/order_confirmed_status "processing" -q -c
-bin/magento config:set payment/amwal_payments/allowspecific 0 -q -c
-bin/magento config:set payment/amwal_payments/debug_mode 0 -q -c
-bin/magento config:set payment/amwal_payments/merchant_id_valid 1 -q -c
-bin/magento config:set payment/amwal_payments/create_user_on_order 1 -q -c
-bin/magento config:set payment/amwal_payments/use_base_currency 0 -q -c
-bin/magento config:set payment/amwal_payments/use_system_country_settings 1 -q -c
-bin/magento config:set payment/amwal_payments/merchant_id "sandbox-amwal-e09ee380-d8c7-4710-a6ab-c9b39c7ffd47" -q -c
+bin/magento config:set currency/options/allow SAR,USD --quiet --lock-config
+bin/magento config:set currency/options/base SAR --quiet --lock-config
+bin/magento config:set currency/options/default SAR --quiet --lock-config
+bin/magento config:set currency/options/allow SAR --quiet --lock-config
+bin/magento config:set admin/security/admin_account_sharing 1 --quiet --lock-config
+bin/magento config:set admin/security/use_form_key 0 --quiet --lock-config
+bin/magento config:set cms/wysiwyg/enabled disabled --quiet --lock-config
+bin/magento config:set catalog/search/engine elasticsearch7 --quiet --lock-config
+bin/magento config:set catalog/search/elasticsearch7_server_hostname es --quiet --lock-config
+bin/magento config:set catalog/search/elasticsearch7_server_port 9200 --quiet --lock-config
+bin/magento config:set catalog/search/elasticsearch7_server_timeout 60 --quiet --lock-config
+bin/magento config:set payment/amwal_payments/active 1 --quiet --lock-config
+bin/magento config:set payment/amwal_payments/dark_mode 0 --quiet --lock-config
+bin/magento config:set payment/amwal_payments/order_confirmed_status "processing" --quiet --lock-config
+bin/magento config:set payment/amwal_payments/allowspecific 0 --quiet --lock-config
+bin/magento config:set payment/amwal_payments/debug_mode 0 --quiet --lock-config
+bin/magento config:set payment/amwal_payments/merchant_id_valid 1 --quiet --lock-config
+bin/magento config:set payment/amwal_payments/create_user_on_order 1 --quiet --lock-config
+bin/magento config:set payment/amwal_payments/use_base_currency 0 --quiet --lock-config
+bin/magento config:set payment/amwal_payments/use_system_country_settings 1 --quiet --lock-config
+bin/magento config:set payment/amwal_payments/merchant_id "sandbox-amwal-e09ee380-d8c7-4710-a6ab-c9b39c7ffd47" --quiet --lock-config
 echo "Config values set"
 
 echo "Flushing cache"
-bin/magento cache:flush -q -c
+bin/magento cache:flush --quiet
 echo "Cache flushed"
