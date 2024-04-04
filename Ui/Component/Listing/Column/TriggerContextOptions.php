c4878811dedc3dd5a7e21f488f65e308c3d3445f
Ahmed Mardi <Mardi@amwal.tech>
2024-04-04 13:50:29 +0200

    Add Order Position

---
+++ Ui/Component/Listing/Column/TriggerContextOptions.php
@@ -0,0 +1,27 @@
+<?php
+declare(strict_types=1);
+
+namespace Amwal\Payments\Ui\Component\Listing\Column;
+
+use Amwal\Payments\ViewModel\ExpressCheckoutButton;
+use Magento\Framework\Data\OptionSourceInterface;
+
+/**
+ * Class TriggerContextOptions
+ */
+class TriggerContextOptions implements OptionSourceInterface
+{
+    /**
+     * @inheritDoc
+     */
+    public function toOptionArray()
+    {
+        return [
+            ['value' => ExpressCheckoutButton::TRIGGER_CONTEXT_CART, 'label' => __('Cart')],
+            ['value' => ExpressCheckoutButton::TRIGGER_CONTEXT_MINICART, 'label' => __('Mini Cart')],
+            ['value' => ExpressCheckoutButton::TRIGGER_CONTEXT_PRODUCT_DETAIL, 'label' => __('Product Detail Page')],
+            ['value' => ExpressCheckoutButton::TRIGGER_CONTEXT_PRODUCT_LIST, 'label' => __('Product Listing Page')],
+            ['value' => ExpressCheckoutButton::TRIGGER_CONTEXT_REGULAR_CHECKOUT, 'label' => __('Regular Checkout')]
+        ];
+    }
+}
