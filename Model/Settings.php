cf790a59a2a7ba5562217b88538bea234122ea8a
Ahmed Mardi <Mardi@amwal.tech>
2024-05-12 14:02:21 +0300

    Add cron job information and pending payment orders count.

--- Model/Settings.php
+++ Model/Settings.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 namespace Amwal\Payments\Model;
 
 use Amwal\Payments\Model\Config;
+use Magento\Sales\Api\OrderRepositoryInterface;
+use Magento\Framework\Api\SearchCriteriaBuilder;
+use Magento\Cron\Model\ResourceModel\Schedule\CollectionFactory as ScheduleCollectionFactory;
+use Magento\Framework\Serialize\Serializer\Json;
 
 class Settings
 {
@@ -12,18 +16,55 @@ class Settings
      */
     private Config $config;
 
+    /**
+     * @var OrderRepositoryInterface
+     */
+    private OrderRepositoryInterface $orderRepository;
+
+    /**
+     * @var SearchCriteriaBuilder
+     */
+    private SearchCriteriaBuilder $searchCriteriaBuilder;
+
+    /**
+     * @var ScheduleCollectionFactory
+     */
+    private ScheduleCollectionFactory $scheduleCollectionFactory;
+
+    /**
+     * @var Json
+     */
+    private Json $json;
+
+    /**
+     * Constructor
+     *
+     * @param Config $config
+     * @param OrderRepositoryInterface $orderRepository
+     * @param SearchCriteriaBuilder $searchCriteriaBuilder
+     * @param ScheduleCollectionFactory $scheduleCollectionFactory
+     * @param Json $json
+     */
     public function __construct(
-        Config $config
+        Config $config,
+        OrderRepositoryInterface $orderRepository,
+        SearchCriteriaBuilder $searchCriteriaBuilder,
+        ScheduleCollectionFactory $scheduleCollectionFactory,
+        Json $json
     ) {
         $this->config = $config;
+        $this->orderRepository = $orderRepository;
+        $this->searchCriteriaBuilder = $searchCriteriaBuilder;
+        $this->scheduleCollectionFactory = $scheduleCollectionFactory;
+        $this->json = $json;
     }
 
     /**
      * Retrieves settings.
      *
-     * @return array An associative array of settings.
+     * @return string
      */
-    public function getSettings() : array
+    public function getSettings(): string
     {
         $settings = [
             'amwal_payment' => $this->config->isActive(),
@@ -40,6 +81,30 @@ class Settings
             'debug' => $this->config->isDebugModeEnabled(),
             'sentry' => $this->config->isSentryReportEnabled()
         ];
-        return $settings;
+
+        try {
+            $pendingPaymentOrders = $this->orderRepository->getList(
+                $this->searchCriteriaBuilder
+                    ->addFilter('payment_method', 'amwal_payment')
+                    ->addFilter('status', 'pending_payment')
+                    ->create()
+            );
+            $settings['pending_payment_orders'] = $pendingPaymentOrders->getTotalCount();
+        } catch (\Exception $e) {
+            $settings['pending_payment_orders'] = 0;
+        }
+
+        // Fetch cron job information
+        $scheduleCollection = $this->scheduleCollectionFactory->create();
+        $scheduleCollection->addFieldToFilter('job_code', 'amwal_pending_orders_update');
+        $scheduleCollection->setOrder('executed_at', 'desc');
+        $schedule = $scheduleCollection->getFirstItem();
+        if ($schedule) {
+            $settings['cronjob_last_run'] = $schedule->getExecutedAt();
+            $settings['cronjob_status'] = $schedule->getStatus();
+            $settings['cronjob_status_message'] = $schedule->getMessages();
+        }
+
+        return $this->json->serialize($settings);
     }
 }
