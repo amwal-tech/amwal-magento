16d360c13d6826b8be420955c87f7e4b3d004f50
Ahmed Mardi <Mardi@amwal.tech>
2024-05-29 15:28:03 +0300

    integration-tests-pre-install.sh update

--- .github/workflows/config/integration-tests-pre-install.sh
+++ .github/workflows/config/integration-tests-pre-install.sh
@@ -4,9 +4,7 @@ composer require tddwizard/magento2-fixtures:^1.1 --no-update
 composer require mockery/mockery:^1.6.11 --no-update
 
 echo "Running PHPUnit with coverage..."
-ls -lh
-cd ~
-ls -lh
-echo $MAGENTO_ROOT
-
-cd ~ && cd $MAGENTO_ROOT/dev/tests/integration && ../../../vendor/bin/phpunit -c phpunit.xml --coverage-clover=coverage.xml
+ls -la
+echo "1..."
+cd local-source
+ls -la
