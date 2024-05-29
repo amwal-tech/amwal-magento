e66ca6083c7e7593fa9aa9597711d24b37c3a970
Ahmed Mardi <Mardi@amwal.tech>
2024-05-29 15:16:12 +0300

    integration-tests-pre-install.sh update

--- .github/workflows/config/integration-tests-pre-install.sh
+++ .github/workflows/config/integration-tests-pre-install.sh
@@ -4,4 +4,5 @@ composer require tddwizard/magento2-fixtures:^1.1 --no-update
 composer require mockery/mockery:^1.6.11 --no-update
 
 echo "Running PHPUnit with coverage..."
-php vendor/bin/phpunit -c dev/tests/integration/phpunit.xml --coverage-clover=coverage.xml
+ls -lh
+find . -name phpunit
