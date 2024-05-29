1564d728a721ed44f5777f599699b692f4dd586a
Ahmed Mardi <Mardi@amwal.tech>
2024-05-29 15:51:54 +0300

    integration-tests-pre-install.sh update

--- .github/workflows/config/integration-tests-pre-install.sh
+++ .github/workflows/config/integration-tests-pre-install.sh
@@ -5,5 +5,5 @@ composer require mockery/mockery:^1.6.11 --no-update
 
 echo "Running PHPUnit with coverage..."
 tail -n 1 ../../../entrypoint.sh
-sed -i 's/../../../vendor/bin/phpunit -c phpunit.xml/../../../vendor/bin/phpunit -c phpunit.xml --coverage-clover=coverage.xml/g' ../../../entrypoint.sh
+sed -i 's|-c phpunit.xml|-c phpunit.xml --coverage-clover=coverage.xml|' ../../../entrypoint.sh
 tail -n 1 ../../../entrypoint.sh
