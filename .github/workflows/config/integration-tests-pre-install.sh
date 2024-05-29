202875fd0da3b4616e15d69a7c984aaa5aec1072
Ahmed Mardi <Mardi@amwal.tech>
2024-05-29 16:15:29 +0300

    integration-tests-pre-install.sh update

--- .github/workflows/config/integration-tests-pre-install.sh
+++ .github/workflows/config/integration-tests-pre-install.sh
@@ -7,6 +7,6 @@ echo "Updating entrypoint.sh to include code coverage reporting..."
 sed -i 's|-c phpunit.xml|-c phpunit.xml --coverage-clover=coverage.xml|' ../../../entrypoint.sh
 tail -n 1 ../../../entrypoint.sh
 echo "Updated phpunit.xml"
-cp local-source/__extdn_github-actions-m2/.dev-tools/tests/integration/etc/phpunit.xml ../../../docker-files/phpunit.xml
+cp local-source/__extdn_github-actions-m2/.dev-tools/tests/integration/phpunit.xml ../../../docker-files/phpunit.xml
 tail -n 200 ../../../docker-files/phpunit.xml
 cd ~
