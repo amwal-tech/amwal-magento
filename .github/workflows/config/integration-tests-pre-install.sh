e8f267cdd9a148f610e177b6cf0e6eff4c95b209
Ahmed Mardi <Mardi@amwal.tech>
2024-05-29 16:32:24 +0300

    integration-tests-pre-install.sh update

--- .github/workflows/config/integration-tests-pre-install.sh
+++ .github/workflows/config/integration-tests-pre-install.sh
@@ -3,8 +3,6 @@ echo "Adding dev packages for integration test"
 composer require tddwizard/magento2-fixtures:^1.1 --no-update
 composer require mockery/mockery:^1.6.11 --no-update
 
-echo "Updating entrypoint.sh to include code coverage reporting..."
+echo "Updating entrypoint.sh and phpunit.xml to include code coverage reporting..."
 sed -i 's|-c phpunit.xml|-c phpunit.xml --coverage-cobertura=cobertura.xml|' ../../../entrypoint.sh
-
-echo "Updated phpunit.xml"
 cp local-source/__extdn_github-actions-m2/.dev-tools/tests/integration/phpunit.xml ../../../docker-files/phpunit.xml
