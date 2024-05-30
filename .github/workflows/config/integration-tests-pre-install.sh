ae5e31bbb6b3121774e99377d1aaff8e4fdbb882
Ahmed Mardi <Mardi@amwal.tech>
2024-05-30 10:47:53 +0300

    integration-tests-pre-install.sh update

--- .github/workflows/config/integration-tests-pre-install.sh
+++ .github/workflows/config/integration-tests-pre-install.sh
@@ -4,13 +4,24 @@ composer require tddwizard/magento2-fixtures:^1.1 --no-update
 composer require mockery/mockery:^1.6.11 --no-update
 
 echo "Adding xdebug extension"
+PHP_VERSION=$(php -r 'echo PHP_VERSION;')
 if ! php -m | grep -i xdebug > /dev/null; then
-    pecl install xdebug
+    if [[ "$PHP_VERSION" == 7.4.* ]]; then
+        echo "PHP version is 7.4.x, installing xdebug-2.9.8"
+        pecl install xdebug-2.9.8
+    else
+        echo "PHP version is $PHP_VERSION, installing the latest xdebug version"
+        pecl install xdebug
+    fi
     docker-php-ext-enable xdebug
+else
+    echo "Xdebug is already installed"
 fi
-# Add xdebug configuration to php.ini
-echo "Configuring xdebug"
-echo "
+
+# Check if xdebug configuration exists
+if ! grep -q "zend_extension=xdebug.so" /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini; then
+    echo "Configuring xdebug"
+    echo "
 [xdebug]
 zend_extension=xdebug.so
 xdebug.mode=coverage
@@ -20,6 +31,9 @@ xdebug.client_host=host.docker.internal
 xdebug.client_port=9003
 xdebug.idekey=PHPSTORM
 " >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
+else
+    echo "Xdebug configuration already exists"
+fi
 
 echo "Updating entrypoint.sh and phpunit.xml to include code coverage reporting..."
 sed -i 's|-c phpunit.xml|-c phpunit.xml --coverage-cobertura=cobertura.xml|' ../../../entrypoint.sh
