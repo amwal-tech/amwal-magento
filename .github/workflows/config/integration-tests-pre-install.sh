9dd7226e378ea66b85d010fe1f835476339ff65e
Ahmed Mardi <Mardi@amwal.tech>
2024-06-03 11:22:17 +0300

    WIP

--- .github/workflows/config/integration-tests-pre-install.sh
+++ .github/workflows/config/integration-tests-pre-install.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
+
+# find all files entrypoint.sh
+find / -name entrypoint.sh -type f
+
+cd ~
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 echo "Adding dev packages for integration test"
 composer require tddwizard/magento2-fixtures:^1.1 --no-update
 composer require mockery/mockery:^1.6.11 --no-update
@@ -42,6 +62,7 @@ ls -la ../../../docker-files
 echo "files"
 ls -la ../../../
 sed -i 's|-c phpunit.xml|-c phpunit.xml --coverage-cobertura=cobertura.xml \&\& mkdir -p /home/coverage \&\& cp -r coverage /home/coverage|' ../../../entrypoint.sh
+
 tail -n 1 ../../../entrypoint.sh
 cp local-source/__extdn_github-actions-m2/.dev-tools/tests/integration/phpunit.xml ../../../docker-files/phpunit.xml
 
