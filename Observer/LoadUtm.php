e93bd0af8c24f0f6572ad07fec640d8de2463449
Ahmed Mardi <141551822+ahmardi@users.noreply.github.com>
2024-09-04 15:50:53 +0300

    UTM Tracking (#387)
    * UTM Tracking
    
    * use SuppressWarnings

---
+++ Observer/LoadUtm.php
@@ -0,0 +1,59 @@
+<?php
+declare(strict_types=1);
+
+namespace Amwal\Payments\Observer;
+
+use Magento\Framework\Event\ObserverInterface;
+use Magento\Framework\Event\Observer;
+use Magento\Framework\Stdlib\CookieManagerInterface;
+use Magento\Framework\Stdlib\Cookie\CookieMetadataFactory;
+use Magento\Framework\Stdlib\Cookie\PublicCookieMetadata;
+use Magento\Framework\App\RequestInterface;
+
+class LoadUtm implements ObserverInterface
+{
+    private CookieManagerInterface $cookieManager;
+    private CookieMetadataFactory $cookieMetadataFactory;
+    private RequestInterface $request;
+
+    public function __construct(
+        CookieManagerInterface $cookieManager,
+        CookieMetadataFactory $cookieMetadataFactory,
+        RequestInterface $request
+    ) {
+        $this->cookieManager = $cookieManager;
+        $this->cookieMetadataFactory = $cookieMetadataFactory;
+        $this->request = $request;
+    }
+
+    /**
+     * Execute observer
+     *
+     * @param Observer $observer
+     * @return void
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
+    public function execute(Observer $observer)
+    {
+        $utmParameters = [
+            'utm_source' => $this->request->getParam('utm_source'),
+            'utm_contact' => $this->request->getParam('utm_contact'),
+            'utm_medium' => $this->request->getParam('utm_medium'),
+            'utm_campaign' => $this->request->getParam('utm_campaign'),
+            'utm_term' => $this->request->getParam('utm_term'),
+            'utm_content' => $this->request->getParam('utm_content')
+        ];
+
+        // Set cookies for each UTM parameter
+        foreach ($utmParameters as $key => $value) {
+            if ($value) {
+                $metadata = $this->cookieMetadataFactory->createPublicCookieMetadata()
+                    ->setDuration(3600) // 1 hour duration
+                    ->setPath('/')
+                    ->setHttpOnly(false);
+
+                $this->cookieManager->setPublicCookie($key, $value, $metadata);
+            }
+        }
+    }
+}
---
+++ Observer/SaveUtm.php
@@ -0,0 +1,89 @@
+<?php
+declare(strict_types=1);
+
+namespace Amwal\Payments\Observer;
+
+use Amwal\Payments\Model\AmwalClientFactory;
+use GuzzleHttp\Exception\GuzzleException;
+use GuzzleHttp\RequestOptions;
+use Magento\Framework\Event\ObserverInterface;
+use Magento\Framework\Event\Observer;
+use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\Framework\Stdlib\CookieManagerInterface;
+use Magento\Sales\Api\OrderRepositoryInterface;
+use Magento\Framework\Json\Helper\Data as JsonHelper;
+
+class SaveUtm implements ObserverInterface
+{
+    private CookieManagerInterface $cookieManager;
+    private OrderRepositoryInterface $orderRepository;
+    private JsonHelper $jsonHelper;
+    private AmwalClientFactory $amwalClientFactory;
+
+    public function __construct(
+        CookieManagerInterface   $cookieManager,
+        OrderRepositoryInterface $orderRepository,
+        JsonHelper               $jsonHelper,
+        AmwalClientFactory       $amwalClientFactory
+    )
+    {
+        $this->cookieManager = $cookieManager;
+        $this->orderRepository = $orderRepository;
+        $this->jsonHelper = $jsonHelper;
+        $this->amwalClientFactory = $amwalClientFactory;
+    }
+
+    /**
+     * @param Observer $observer
+     * @throws NoSuchEntityException
+     */
+    public function execute(Observer $observer)
+    {
+        $order = $observer->getOrder();
+
+        $utmParameters = [
+            'utm_source' => $this->cookieManager->getCookie('utm_source'),
+            'utm_contact' => $this->cookieManager->getCookie('utm_contact'),
+            'utm_medium' => $this->cookieManager->getCookie('utm_medium'),
+            'utm_campaign' => $this->cookieManager->getCookie('utm_campaign'),
+            'utm_term' => $this->cookieManager->getCookie('utm_term'),
+            'utm_content' => $this->cookieManager->getCookie('utm_content')
+        ];
+
+        $order->setData('amwal_utm', $this->jsonHelper->jsonEncode($utmParameters));
+
+        $this->orderRepository->save($order);
+
+        $this->sendUtmToAmwal($order->getAmwalOrderId(), $utmParameters);
+    }
+
+
+    /**
+     * Send UTM parameters to Amwal API
+     * @param string $amwalOrderId
+     * @param array $amwalUtm
+     *
+     * @throws NoSuchEntityException
+     */
+    private function sendUtmToAmwal(string $amwalOrderId, array $amwalUtm): void
+    {
+        $amwalClient = $this->amwalClientFactory->create();
+        $orderDetails = [];
+        $orderDetails['order_utm'] = $amwalUtm;
+        try {
+            $amwalClient->post(
+                'transactions/' . $amwalOrderId . '/set_order_details',
+                [
+                    RequestOptions::JSON => $orderDetails
+                ]
+            );
+        } catch (GuzzleException $e) {
+            $message = sprintf(
+                'Failed to set UTM parameters for order %s: %s',
+                $amwalOrderId,
+                $e->getMessage()
+            );
+            throw new RuntimeException($message);
+        }
+    }
+}
