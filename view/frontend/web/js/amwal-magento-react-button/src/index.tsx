92b57dadec4032733be436752a3dd2871af58247
ahmardi <Mardi@amwal.tech>
2025-04-13 16:25:02 +0200

    Add installments-timeline

--- view/frontend/web/js/amwal-magento-react-button/src/index.tsx
+++ view/frontend/web/js/amwal-magento-react-button/src/index.tsx
@@ -1,3 +1,9 @@
 import AmwalMagentoReactButton from './AmwalMagentoReactButton'
+import AmwalMagentoInstallmentsTimeline from './AmwalMagentoInstallmentsTimeline'
+
+export {
+    AmwalMagentoReactButton,
+    AmwalMagentoInstallmentsTimeline
+}
 
 export default AmwalMagentoReactButton
