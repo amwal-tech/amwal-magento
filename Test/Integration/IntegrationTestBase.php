77f00b3867c3afa7852966caa9c56bcfef1a7302
Ahmed <Mardi@amwal.tech>
2025-11-18 13:41:35 +0200

    Update product fixture price in integration test
    Changed the mock product price from 101.25 to 96.25 in IntegrationTestBase to reflect updated test requirements.

--- Test/Integration/IntegrationTestBase.php
+++ Test/Integration/IntegrationTestBase.php
@@ -120,7 +120,7 @@ class IntegrationTestBase extends TestCase
             $this->productFixture = new ProductFixture(
                 ProductBuilder::aSimpleProduct()
                     ->withSku(self::MOCK_PRODUCT_SKU)
-                    ->withPrice(101.25)
+                    ->withPrice(96.25)
                     ->build()
             );
         }
